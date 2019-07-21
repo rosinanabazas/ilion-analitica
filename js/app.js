56ab97acb3ce9908f3a6c986acc9014a112ac8ef
Rosina Nabazas <rosinanabazas@gmail.com>
2019-07-21 14:58:01 -0300

    Ajustes de los fondos

--- js/app.js
+++ js/app.js
@@ -10,3 +10,54 @@ $(".smooth-scroll").click(function() {
 $(".show").click(function(){
   $(".hidden-mobile-nav").toggleClass("show-nav");
 });
+
+
+
+var slideShow = function(container, time, effect) {
+  container = document.querySelector(container);
+  this.images = [];
+  this.curImage = 0;
+
+  if (effect === "fade") {
+    for (i = 0; i < container.childElementCount; i++) {
+      this.images.push(container.children[i]);
+      this.images[i].style.opacity = 0;
+    }
+
+    // Handle going to to the next slide
+    var nextSlide = function() {
+      for (var i = 0; i < this.images.length; i++) {
+        if (i != this.curImage) this.images[i].style.opacity = 0;
+      }
+      this.images[this.curImage].style.opacity = 1;
+      this.curImage++;
+      if (this.curImage >= this.images.length) {
+        this.curImage = 0;
+      }
+      window.setTimeout(nextSlide.bind(document.getElementById(this)), time);
+    };
+    nextSlide.call(this);
+  } else if (effect === "clickFade") {
+    for (i = 0; i < container.childElementCount; i++) {
+      this.images.push(container.children[i]);
+      this.images[i].style.opacity = 0;
+    }
+
+    // Handle going to to the next slide
+    var nextSlideClick = function() {
+      for (var i = 0; i < this.images.length; i++) {
+        if (i != this.curImage) this.images[i].style.opacity = 0;
+      }
+      this.images[this.curImage].style.opacity = 1;
+      this.curImage++;
+
+      if (this.curImage >= this.images.length) {
+        this.curImage = 0;
+      }
+      window.setTimeout(nextSlideClick.bind(document.getElementById(this)), time);
+    };
+    nextSlideClick.call(this);
+  }
+
+};
+slideShow(".slideshow", 5000, "fade");
